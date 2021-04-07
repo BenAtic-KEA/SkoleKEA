a7efd078448fd6d6a68a999460c9c54ca9644458
Mark Hansen <Mark024e@stud.kea.dk>
2021-04-07 17:41:36 +0200

    x

---
+++ SkoleKEA/src/inheritance_Files_aflevering/Exercise3/FileCounter.java
@@ -0,0 +1,40 @@
+package inheritance_Files_aflevering.Exercise3;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+
+public class FileCounter {
+    public static void main(String[] args)
+            throws FileNotFoundException {
+
+        // lokalisere min txt fil.
+        File howManyWords = new File("SkoleKEA/Resources/HowManyWords.txt");
+        // laver en scanner der læser min fil
+        Scanner fileReader = new Scanner(howManyWords);
+
+        System.out.println(howManyWords(fileReader));
+
+    }
+
+    // laver en metode der tæller ordene i filen.
+    public static int howManyWords(Scanner file)
+            throws FileNotFoundException{
+
+        int count = 0;
+        // et loop der køre så længe der er en ekstra linje.
+        while(file.hasNextLine()) {
+            // gemmer linjen i Current Line, så jeg kan lægge den ind i et string Array
+            String currentLine = file.nextLine();
+            String[] wordsInLine = currentLine.split(" ");
+
+            // jeg laver et loop for hvert index i mit array hvor jeg lægger 1 til "count"
+            for (int i = 0; i < wordsInLine.length; i++)
+
+                count++;
+        }
+
+        return count;
+    }
+
+}
