8425085e4f5d122fdf36b6b3d3990379670bdb55
Mark Hansen <Mark024e@stud.kea.dk>
2021-05-02 20:23:12 +0200

    x

--- SkoleKEA/src/Exceptions/opgaver/NemIdAuthorizer.java
+++ SkoleKEA/src/Exceptions/opgaver/NemIdAuthorizer.java
@@ -11,32 +11,24 @@ public class NemIdAuthorizer {
     public static void main(String[] args) throws FileNotFoundException{
 
         boolean correctInformation = false;
-            while(!correctInformation) {
-                try {
+        while(!correctInformation) {
+            try {
+                Scanner userInput = new Scanner(System.in);
+                System.out.println("CprNr:");
+                String cprNr = userInput.nextLine();
+                System.out.println("Password between 6 and 40 characters");
+                String password = userInput.nextLine();
+                isValidInput(cprNr, password);
+                System.out.println("User now logged in");
+                correctInformation = isValidInput(cprNr, password);
 
-                    Scanner userInput = new Scanner(System.in);
-                    System.out.println("CprNr:");
-                    String cprNr = userInput.nextLine();
-                    System.out.println("Password between 6 and 40 characters");
-                    String password = userInput.nextLine();
-                    isValidInput(cprNr, password);
-                    if(PasswordAuth.isValidPassword(cprNr,password)) {
-                        System.out.println("User now logged in");
-                        correctInformation = isValidInput(cprNr, password);
-                    } else{
-                        System.out.println("Password not allowed");
-                    }
-
-                } catch (InputMismatchException e) {
-                    e.printStackTrace();
-
-                    System.out.println("Wrong length");
-                }catch (NoSuchUserException e){
-
-                    System.out.println("User does not exist");
-                }
-
+            } catch (InputMismatchException e) {
+                e.printStackTrace();
+                System.out.println("Wrong length");
+            }catch (NoSuchUserException e){
+                System.out.println("User does not exist");
             }
+        }
     }
 
     public static boolean isValidInput(String cprNr, String password) throws InputMismatchException, FileNotFoundException {
@@ -44,8 +36,7 @@ public class NemIdAuthorizer {
         if (cprNr.length() != 10) {
             throw new InputMismatchException();
         }
-
-        /*for (int i = 0; i < brugerListe().size(); i++) {
+        for (int i = 0; i < brugerListe().size(); i++) {
             if (cprNr.equals(brugerListe().get(i).getUsername())) {
                 if (password.equals(brugerListe().get(i).getPassword())) {
                     return true;
@@ -53,36 +44,24 @@ public class NemIdAuthorizer {
             }
         }
         throw new NoSuchUserException();
-    */
-        return true;
     }
 
 
     public static ArrayList<NemIdUser> brugerListe() throws FileNotFoundException {
-
         File users = new File("SkoleKEA/Resources/brugere.csv");
-
         ArrayList<NemIdUser> userList = new ArrayList<>();
-
         Scanner fileReader = new Scanner(users);
-
         String[] specificUser;
-
         fileReader.nextLine();
-        while(fileReader.hasNextLine()){
 
+        while(fileReader.hasNextLine()){
             String currentLine = fileReader.nextLine();
             specificUser = currentLine.split(";");
-
             String currentUsername = specificUser[0];
             String currentPassword = specificUser[1];
-
             NemIdUser currentUser = new NemIdUser(currentUsername,currentPassword);
             userList.add(currentUser);
-
         }
         return userList;
     }
-
-
 }
