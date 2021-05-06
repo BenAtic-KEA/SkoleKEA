a9ea0853539cdc0765e376d4407d5adad2b95d02
BenAtic-KEA <78417528+BenAtic-KEA@users.noreply.github.com>
2021-05-06 11:45:59 +0200

    x

---
+++ SkoleKEA/src/Comparable_And_UnitTest/Person.java
@@ -0,0 +1,34 @@
+package Comparable_And_UnitTest;
+
+public class Person implements Comparable<Person>{
+
+    private String name;
+    private double height;
+    private int age;
+
+    public Person(String name, double height,int age){
+        this.name = name;
+        this.height = height;
+        this.age = age;
+
+    }
+
+
+    @Override
+    public int compareTo(Person other) {
+
+        if(this.age > other.age) {
+            return 1;
+        } else if(this.age < other.age){
+            return -1;
+        } else {
+            if(this.height > other.height){
+                return 1;
+            } else if (this.height < other.height){
+                return -1;
+            } else{
+                return 0;
+            }
+        }
+    }
+}
